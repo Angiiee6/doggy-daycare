5f3416efe873ccd59aaa55553f033f54038bb914
angie <angelica.ekman@hotmail.com>
2025-09-26 05:03:24 +0200

    made improvements in about.css, dogs.css

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,19 +9,19 @@ const About = () => {
         <div className="about-container">
 <h2>Hundis - Mer Än Bara ett Hunddagis</h2>
       
-      <p>På Hundis förstår vi att din hund är en riktig familjemedlem. Därför har vi skapat en modern och trygg mötespluts där skratt, svänsande svansar och glada skall är vardagsmat. Vår filosofi är enkel: <strong>en trött hund är en lycklig hund</strong>.</p>
+      <p className='description'>På Hundis förstår vi att din hund är en äkta familjemedlem. Därför har vi skapat en modern och trygg mötespluts där skratt, viftande svansar och glada skall är vardagsmat. Vår filosofi är enkel: <strong>En trött hund är en lycklig hund</strong>.</p>
 
       <h3>Vad erbjuder vi på Hundis?</h3>
       
       <div className="features-grid">
         <div className="feature-item">
           <h4>🐾 Social lek i rastgrupper</h4>
-          <p>Våra hundar delas in i små, välpassande grupper för optimal kamratskap.</p>
+          <p>Våra hundar delas in i små, välanpassande grupper för optimal kamratskap.</p>
         </div>
         
         <div className="feature-item">
           <h4>🌲 Utelek på säkra inhägnader</h4>
-          <p>Rastgårdar byggda för säkerhet och kul utemotion.</p>
+          <p>Rastgårdar byggda för säkerhet och kul motion utomhus.</p>
         </div>
         
         <div className="feature-item">
@@ -31,7 +31,7 @@ const About = () => {
         
         <div className="feature-item">
           <h4>❤️ Massor av kärlek och omtanke</h4>
-          <p>Vårt passionerade team ser till att varje hund mår bra.</p>
+          <p>Vårt passionerade team ser till att varje hund får lika mycket omtanke och mår bra.</p>
         </div>
       </div>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,11 +7,13 @@ const Home = () => {
     return (
             <div className="home-container">
                 <h1>Välkommen till Hundis</h1>
+                <div className='text-wrapper'>
                 <p>På Hundis är varje dag ett äventyr fyllt med lek, kamratskap och kärleksfull omvårdnad. </p>
                     <p>Vi är mer än ett dagis – vi är din hunds gladaste mötesplats. </p>
                     <p>Här får han eller hon springa, leka och vara sig själv i en säker och stimulerande miljö. </p>
                     <p>Välkommen!</p>
             </div>
+        </div>
     );
 };
 
